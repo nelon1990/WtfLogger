48fa8f1d0fd3836f933ae38a0837d03d1154220b
nelon199 <470174440@qq.com>
2017-02-24 11:09:34 +0800

    修改README

--- README.md
+++ README.md
@@ -2,8 +2,47 @@
 
 [![](https://jitpack.io/v/nelon1990/WtfLogger.svg)](https://jitpack.io/#nelon1990/WtfLogger)
 
-Step 1. Add the JitPack repository to your build file.
-Add it in your root build.gradle at the end of repositories:
+
+## 使用
+链式调用
+###### K-V打印
+
+    WtfLog.d.title("示例")
+                    .tmpTag(TAG)
+                    .key("key1").intVal(1)
+                    .key("key2").stringVal("string1")
+                    .key("key2").floatVal(3.5f)
+                    .key("map").mapVal(mMap)
+                    .key("list").listVal(Arrays.asList(1, 2, 3, 4))
+                    .print();
+
+![](http://p1.bpimg.com/567571/c8705631d14a9466.png)
+
+###### bean打印
+
+    public class User {
+        String username;
+        String password;
+    }
+
+    WtfLog.d.title("示例")
+                    .tmpTag(TAG)
+                    .bean(user)
+                    .print();
+
+![](http://p1.bqimg.com/567571/e6e05133e2e62ac5.png)
+
+###### 线程与堆栈打印
+    WtfLog.d.title("示例")
+                    .tmpTag(TAG)
+                    .stackTrace()
+                    .threadInfo()
+                    .print();
+![](http://i1.piimg.com/567571/9575289ca165962d.png)
+
+
+## 集成
+Step 1. 在你的project的gradle脚本，仓库配置项中增下下发仓库配置；
 
 	allprojects {
 		repositories {
@@ -13,8 +52,8 @@ Add it in your root build.gradle at the end of repositories:
 	}
 
 
-Step 2. Add the dependency
+Step 2. module的gradle脚本中，增加下方依赖
 
 	dependencies {
-	        compile 'com.github.nelon1990:WtfLogger:v1.0'
+	        compile 'com.github.nelon1990:WtfLogger:v1.0.0'
 	}
--- app/src/main/java/pers/nelon/wtflogger/MainActivity.java
+++ app/src/main/java/pers/nelon/wtflogger/MainActivity.java
@@ -3,7 +3,6 @@ package pers.nelon.wtflogger;
 import android.os.Bundle;
 import android.support.v4.util.ArrayMap;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import java.util.Arrays;
 import java.util.Map;
@@ -11,66 +10,56 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private Map<String, String> mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mMap = new ArrayMap<>();
-        mMap.put("nelon1", "23333");
-        mMap.put("nelon2", "23333");
-        mMap.put("nelon3", "23333");
-        mMap.put("nelon4", "23333");
 
-        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><PARAM><DBID>35</DBID><SEQUENCE>atgtca</SEQUENCE><MAXNS>10</MAXNS><MINIDENTITIES>90</MINIDENTITIES><MAXEVALUE>10</MAXEVALUE><USERNAME>admin</USERNAME><PASSWORD>111111</PASSWORD><TYPE>P</TYPE><RETURN_TYPE>2</RETURN_TYPE></PARAM>";
-        String json = "{\n" +
-                "    \"glossary\": {\n" +
-                "        \"title\": \"example glossary\",\n" +
-                "        \"GlossDiv\": {\n" +
-                "            \"title\": \"S\",\n" +
-                "            \"GlossList\": {\n" +
-                "                \"GlossEntry\": {\n" +
-                "                    \"ID\": \"SGML\",\n" +
-                "                    \"SortAs\": \"SGML\",\n" +
-                "                    \"GlossTerm\": \"Standard Generalized Markup Language\",\n" +
-                "                    \"Acronym\": \"SGML\",\n" +
-                "                    \"Abbrev\": \"ISO 8879:1986\",\n" +
-                "                    \"GlossDef\": {\n" +
-                "                        \"para\": \"A meta-markup language, used to create markup languages such as DocBook.\",\n" +
-                "                        \"GlossSeeAlso1\": [\n" +
-                "                            \"GML\",\n" +
-                "                            \"XML\"\n" +
-                "                        ]\n" +
-                "                    },\n" +
-                "                    \"GlossSee\": \"markup\"\n" +
-                "                }\n" +
-                "            }\n" +
-                "        }\n" +
-                "    }\n" +
-                "}";
+        Map<String, String> map = new ArrayMap<>();
+        map.put("map key1", "1");
+        map.put("map key2", "2");
+        map.put("map key3", "3");
+        map.put("map key4", "4");
 
-        WtfLog.d.title("test json")
-                .tmpTag("json")
+        String json = "{\"employees\":[{\"firstName\":\"Bill\",\"lastName\":\"Gates\"},{\"firstName\":\"George\",\"lastName\":\"Bush\"},{\"firstName\":\"Thomas\",\"lastName\":\"Carter\"}]}";
+
+        WtfLog.d.title("示例")
+                .tmpTag(TAG)
                 .json(json)
-                .key("asda").intVal(23333)
                 .print();
-    }
 
-    public void print(View view) {
-        WtfLog.d
-                .addKvStr("asas");
+        WtfLog.d.title("示例")
+                .tmpTag(TAG)
+                .key("key1").intVal(1)
+                .key("key2").stringVal("string1")
+                .key("key2").floatVal(3.5f)
+                .key("map").mapVal(map)
+                .key("list").listVal(Arrays.asList(1, 2, 3, 4))
+                .print();
 
-        WtfLog.d.tmpTag("test")
-                .title("test WtfLogger")
+        User user = new User();
+        user.username = "nelon";
+        user.password = "123456";
+
+        WtfLog.d.title("示例")
+                .tmpTag(TAG)
+                .bean(user)
+                .print();
+
+        WtfLog.d.title("示例")
+                .tmpTag(TAG)
+                .stackTrace()
                 .threadInfo()
-                .stackTrace(999)
-                .key("aaa").intVal(30)
-                .key("list").listVal(Arrays.asList(1, 2, 3, 4, "adasdas"))
-                .key("map").mapVal(mMap)
-                .key("currentTimeMillis").longVal(System.currentTimeMillis())
                 .print();
 
     }
+
+
+    public class User {
+        String username;
+        String password;
+    }
+
 }
